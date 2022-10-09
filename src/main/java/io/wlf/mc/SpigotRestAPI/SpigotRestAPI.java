1e7eda5ccd8e95f2a6416c5f46850be328639827
charmines <47259933+ilikestohack@users.noreply.github.com>
2022-10-09 14:50:23 -0400

    Update SpigotRestAPI.java

--- src/main/java/io/wlf/mc/SpigotRestAPI/SpigotRestAPI.java
+++ src/main/java/io/wlf/mc/SpigotRestAPI/SpigotRestAPI.java
@@ -75,7 +75,7 @@ public class SpigotRestAPI extends JavaPlugin {
         initControllers();
         initListeners();
         if(this.config.getBoolean("httpEvents.enabled", false)) {
-            this.httpEventService.fire(HttpEventType.server, HttpEvent.online, null);
+            this.httpEventService.fire(HttpEventType.server, HttpEvent.online, "{'post': 'true'}");
         }
     }
 
@@ -90,7 +90,7 @@ public class SpigotRestAPI extends JavaPlugin {
     @Override
     public void onDisable() {
         if(this.config.getBoolean("httpEvents.enabled", false)) {
-            this.httpEventService.fire(HttpEventType.server, HttpEvent.offline, null);
+            this.httpEventService.fire(HttpEventType.server, HttpEvent.offline, "{'post': 'true'}");
         }
         
         stop();
